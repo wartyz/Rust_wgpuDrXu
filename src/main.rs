d9accde3a1c35e3bd0b131246e844119a14f0762
wartyz <zwenderk1@hotmail.com>
2023-04-05 20:05:16 +0200

    Fin capítulo 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+// https://www.youtube.com/watch?v=LULQtc5CUJ8&list=PL_UrKDEhALdJS0VrLPn7dqC5A4W1vCAUT&index=2
+
+
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop).unwrap();
+    window.set_title("Mi ventana");
+    env_logger::init();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        }
+    });
 }
